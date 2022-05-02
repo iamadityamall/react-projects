b2231719e8267c6bc9295c5626b0d03e6b9d8149
Aditya Mall <contact.adityamall@gmail.com>
2022-05-02 14:15:26 +0530

    disabled join button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,17 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-      <section className="h-[75vh] bg-[#F5F5F5] flex flex-col justify-center items-center px-10 font-mono lg:flex-row">
-        <div className="space-y-10 lg:w-[40vw] lg:p-3">
+      <section className="h-[75vh] bg-[#F5F5F5] flex flex-col justify-center items-center px-10 font-mono lg:flex-row lg:space-x-5">
+        <div className="space-y-10 lg:w-[50vw] lg:p-3">
           <h1 className="text-4xl xl:text-6xl">React Projects</h1>
           <p className="text-xl leading-relaxed">
             Projects are the most practical way to learn any language, and{" "}
             <span className="italic font-bold ">React</span> is no exception.
             Solidify your knowledge of React by creating these cool projects.
           </p>
-          <button className="p-4 border-2 border-black">
+          {/* <button className="p-4 border-2 border-black">
             Join the course{" "}
-          </button>
+          </button> */}
         </div>
         <div className="hidden xl:flex xl:w-[30vw]">
           <img
