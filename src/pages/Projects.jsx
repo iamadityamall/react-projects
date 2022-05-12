0792daa14bb99a582fe5dce2b4faf79596e870f1
Aditya Mall <contact.adityamall@gmail.com>
2022-05-12 22:09:13 +0530

    added margin to project section in x direction

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
     document.title = "react-projects";
   }, []);
 
-  return <section className='flex flex-col items-center space-y-5 my-10'>
+  return <section className='flex flex-col items-center space-y-5 my-10 mx-10'>
     {projects.map((data) => {
       return <SingleProject data={data} key={data.id}/>
     })}
