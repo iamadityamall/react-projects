5d4678a876b9190e55ce8fc0e6ea65023e0f355d
Aditya Mall <contact.adityamall@gmail.com>
2022-05-12 22:20:32 +0530

    width updated

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 const SingleProject = ({ data }) => {
   const { name, info, link, image } = data;
   console.log(name);
@@ -9,13 +8,9 @@ const SingleProject = ({ data }) => {
       href={link}
       target="_blank"
       rel="noreferrer"
-      className="flex flex-col w-96 rounded-lg"
+      className="flex flex-col w-[80vw] rounded-lg sm:w-[60vw] md:w-[45vw] lg:w-[30vw] xl:w-[20vw]"
     >
-      <img
-        src={image}
-        alt={name}
-        className="w-full object-cover h-32"
-      />
+      <img src={image} alt={name} className="w-full object-cover h-32" />
       <div className="flex flex-col p-4 bg-slate-300 h-full">
         <h4 className="font-bold">{name}</h4>
         <p className="pt-2 h-full">{info}</p>
